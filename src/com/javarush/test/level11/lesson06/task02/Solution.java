0e63336efa65429266828f2580a2d11e4b3cd1e5
Aspirant89 <aspirant89@inbox.ru>
2016-06-13 22:37:06 +0300

    vse zadachi lesson6/lvl11

---
+++ src/com/javarush/test/level11/lesson06/task02/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level11.lesson06.task02;
+
+/* Домашние животные
+Написать три класса: Pet (домашнее животное), Cat(кот) и Dog(собака).
+Унаследовать кота и собаку от животного.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Pet
+    {
+        public Pet() {
+
+        }
+    }
+
+    public class Cat extends Pet
+    {
+        public Cat() {
+
+        }
+    }
+
+    public class Dog extends  Pet
+    {
+        public Dog() {
+
+        }
+    }
+}
