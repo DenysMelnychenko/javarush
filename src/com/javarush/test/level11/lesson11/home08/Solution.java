f5953608faf3d8abbbfcc1c006d77c380c8fb8a2
FynnyByte <aspirant89@inbox.ru>
2016-06-17 15:24:07 +0300

    merge tablet with pc

---
+++ src/com/javarush/test/level11/lesson11/home08/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level11.lesson11.home08;
+
+/* Третья правильная «цепочка наследования»
+Расставь правильно «цепочку наследования» в классах: Pet (домашнее животное), Cat (кот), Dog(собака), Car (машина).
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Pet
+    {
+        public Pet() {
+
+        }
+    }
+
+    public class Cat extends Pet
+    {
+        public Cat() {
+
+        }
+    }
+
+    public class Car
+    {
+        public Car() {
+
+        }
+    }
+
+    public class Dog extends Pet
+    {
+        public Dog() {
+
+        }
+    }
+}
